b11fc8f01f66fa14c606797394cf810901830518
Citdx <33003361+Citdx@users.noreply.github.com>
2019-03-01 20:15:41 -0500

    Removed credits
    Well that's gone...

--- GetService.lua
+++ GetService.lua
@@ -3,4 +3,3 @@ local chat = game:GetService('Chat')
 local Data = game:GetService('DataStoreService')
 local badgeServ = game:GetService('BadgeService')
 local sound = game:GetService('SoundService')
--- Made by Citdx
