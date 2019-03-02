ffb4a6c751ee605110f01f3e96865594e9c16aa0
Citdx <33003361+Citdx@users.noreply.github.com>
2019-03-01 20:21:59 -0500

    Update warn.lua

--- warn.lua
+++ warn.lua
@@ -2,6 +2,6 @@ warn('Warning messages are a way for your game to indicate that something is wro
 wait(2)
 warn("It's really useful to spot bugs in your games. You can put it like this.")
 
-if pcall(function()) then
+if print('Function differed') then
     warn('Function called!')
 --Normally, the command above would be used as a print() and not a warn().
