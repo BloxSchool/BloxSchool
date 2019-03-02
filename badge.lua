7715dc9b787b4c539269570907e72e505a96bc2e
Citdx <33003361+Citdx@users.noreply.github.com>
2019-03-01 19:59:58 -0500

    Badge Script Added

---
+++ badge.lua
@@ -0,0 +1,24 @@
+--|BadgeScript|--
+    --|ServiceComms|--
+    local Badge = game:GetService('BadgeService')
+    local id = 0000000 --Change to badge ID
+    --|AwardBadge|--
+    local function awardBadge(onPlayerJoin)
+      --|CheckIfOwnBadge|--
+        local player = Players.LocalPlayer
+         local hasBadge = false
+        local success, message = pcall(function()
+            hasBadge = BadgeService:UserHasBadgeAsync(player.UserId,badgeID)
+        end)
+     --|Error|--
+        -- If there's an error, issue a warning and exit the function
+        if not success then
+            warn("Error while checking if player has badge: " .. tostring(message))
+            return
+        end
+     --|BadgeAward|--
+        if hasBadge == false then
+            BadgeService:AwardBadge(player.UserId, badgeID)
+        end
+    end
+--|End|--
