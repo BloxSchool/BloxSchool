2694ef00da8aea34ae8a870c718e8ff087043b00
Citdx <33003361+Citdx@users.noreply.github.com>
2019-03-01 20:02:17 -0500

    Added warn.lua to master branch

---
+++ warn.lua
@@ -0,0 +1,7 @@
+warn('Warning messages are a way for your game to indicate that something is wrong.')
+wait(2)
+warn("It's really useful to spot bugs in your games. You can put it like this.")
+
+if pcall(function()) then
+    warn('Function called!')
+--Normally, the command above would be used as a print() and not a warn().
