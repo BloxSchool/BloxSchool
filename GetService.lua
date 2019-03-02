d384fbd8f7083288c7a886fa82426d6d02744ba9
Citdx <33003361+Citdx@users.noreply.github.com>
2019-03-01 20:15:04 -0500

    Services

---
+++ GetService.lua
@@ -0,0 +1,6 @@
+--|Services for further documentation|--
+local chat = game:GetService('Chat')
+local Data = game:GetService('DataStoreService')
+local badgeServ = game:GetService('BadgeService')
+local sound = game:GetService('SoundService')
+-- Made by Citdx
