3bf782a625600e4fe7fd6b71177c959bc326b7cb
Citdx <33003361+Citdx@users.noreply.github.com>
2019-03-03 10:19:02 -0500

    Folder commands

---
+++ folders.lua
@@ -0,0 +1,16 @@
+Instance.new 'Folder'
+print('Folders are a great way to organize stuff in your roblox games.')
+Folder.Name = 'Parts'
+Folder.Parent = 'Workspace'
+print('Folders also help with organizing scripts');
+Instance.new 'Folder'
+Folder.Name = 'Scripts'
+Folder.Parent = 'Workspace'
+warn('Folders are used to divide parts/scripts into sections.')
+Instance.new 'Part'
+Part.Name = 'Part1'
+Part.Parent = 'Parts'
+Instance.new 'Script'
+Script.Name = 'Script1'
+Script.Parent = 'Scripts'
+--In conclusion, Folders are a great way to divide parts/scripts/models into different sections.
