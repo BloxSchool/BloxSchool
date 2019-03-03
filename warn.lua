98159d532c9beb8c2fa838d7440a1edfee23b564
Landon <33003361+Citdx@users.noreply.github.com>
2019-03-03 10:43:48 -0500

    Conclusion

--- warn.lua
+++ warn.lua
@@ -4,3 +4,5 @@ warn("It's really useful to spot bugs in your games. You can put it like this.")
 
 if print('Song switched!') then
     warn('Song has switched!')
+    
+    --As you can see, warnings are very useful for game development. You can find these warnings by pressing F9 on your keyboard in-game.
