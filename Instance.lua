da567dca675b48c06469a942c9186548e338ce5b
Citdx <33003361+Citdx@users.noreply.github.com>
2019-03-02 19:30:50 -0500

    Instance commands

---
+++ Instance.lua
@@ -0,0 +1,8 @@
+Instance.new "Part"
+print('Instance.new is a great way to add parts and scripts into your game.')
+print('For example, what if I needed to add a part?')
+Run Instance.new "Part" on the command line. --A new part will spawn in and you can mess around with it.
+print('Say I needed a new script?')
+put Instance.new 'Script' into the command line. --Then, you will need to edit the script from Roblox Studio's script editor.
+print('Say I need an Accessory (for Humanoids)? No problem!')
+Instance.new "Accessory" --This adds an accessory which you will then need to mesh.
