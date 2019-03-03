dbfe6b1c3b9af5c5b7f5a2be9a3bdbbca24e118c
Citdx <33003361+Citdx@users.noreply.github.com>
2019-03-02 23:40:46 -0500

    math.lua complete

--- math.lua
+++ math.lua
@@ -3,3 +3,7 @@ math.random(5,32,1,4)
 --Math statements can be used for TextLabels also.
 math.random('5,291,451,75')
 warn('Keep in mind that math.random commands are also apart of the base Lua API.')
+math.random('81,42,1,63')
+--Always make sure to have math.random on when you're making a game called Statement Generator.
+math.random(6,32,743,54)
+print('Goodbye!')
